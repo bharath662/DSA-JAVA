6e2eb6a09c40b2d3235f1b3d22fed85d2d4d3600
bharath662 <bharathmedapatla@gmail.com>
2026-08-14 19:10:37 +0530

    Sync GFG - Row with Max 1s in Rowwise Sorted (Medium)

--- GFG/Medium/0_RowWithMax1sInRowwiseSorted.java
+++ GFG/Medium/0_RowWithMax1sInRowwiseSorted.java
@@ -4,33 +4,27 @@
  * Difficulty   : Medium
  */
 
-class Solution {
-    public int rowWithMax1s(int[][] mat) {
-        int rows = mat.length;
-        int cols = mat[0].length;
-        
-        int maxOnes = 0;
-        int ans = -1;
-        for (int i=0;i<rows;i++){
-            int low = 0;
-            int high = cols -1;
-            
-            while(low <= high){
-                int mid = (low + high)/2;
-                if(mat[i][mid] == 1){
-                    high = mid - 1;
-                }
-                else{
-                    low = mid + 1;
-                }
-            }
-            int ones = cols - low;
+ class Solution {
+ int rowWithMax1s(int arr[][]) {
+     int maxRowIndex = -1;
+     int maxCount = 0;
+     int n=arr.length;
+     int m=arr.length;
 
-            if (ones > maxOnes) {
-                maxOnes = ones;
-                ans = i;
-        }
-    }
-    return ans;
-}
-}
+     for (int i = 0; i < n; i++) {
+         int count = 0;
+         for (int j = 0; j < m; j++) {
+             if (arr[i][j] == 1) {
+                 count++;
+             }
+         }
+
+         if (count > maxCount) {
+             maxCount = count;
+             maxRowIndex = i;
+         }
+     }
+
+     return maxRowIndex;
+ }
+ }
