9c565ec590ac7fd4bc867696635d1fff6d9f4bee
bharath662 <bharathmedapatla@gmail.com>
2026-08-14 12:31:10 +0530

    Sync GFG - Single Among Doubles in a Sorted (Medium)

---
+++ GFG/Medium/0_SingleAmongDoublesInASorted.java
@@ -0,0 +1,16 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/find-the-element-that-appears-once-in-sorted-array0624/1
+ * Platform     : GFG
+ * Difficulty   : Medium
+ */
+
+class Solution {
+    public int single(int[] arr) {
+        int n = arr.length;
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            ans ^= arr[i];
+        }
+        return ans;
+    }
+}
