d56c71dbfcfb5781f299b3ba35add78fbe7744fe
bharath662 <bharathmedapatla@gmail.com>
2026-08-14 12:18:10 +0530

    Sync GFG - Sorted and Rotated Minimum (Easy)

---
+++ GFG/Easy/0_SortedAndRotatedMinimum.java
@@ -0,0 +1,17 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/minimum-element-in-a-sorted-and-rotated-array3611/1
+ * Platform     : GFG
+ * Difficulty   : Easy
+ */
+
+class Solution {
+    public int findMin(int[] arr) {
+        int n = arr.length;
+        int  minVal = Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            minVal=Math.min(minVal,arr[i]);
+        }
+        return minVal;
+    }
+}
+
