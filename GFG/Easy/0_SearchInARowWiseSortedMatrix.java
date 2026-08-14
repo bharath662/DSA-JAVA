b3d78e1d21a95978f69e7975b2454b82ddbd8583
bharath662 <bharathmedapatla@gmail.com>
2026-08-14 19:45:02 +0530

    Sync GFG - Search in a row-wise sorted matrix (Easy)

---
+++ GFG/Easy/0_SearchInARowWiseSortedMatrix.java
@@ -0,0 +1,36 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/search-in-a-row-wise-sorted-matrix/1
+ * Platform     : GFG
+ * Difficulty   : Easy
+ */
+
+class Solution {
+    public boolean searchRowMatrix(int[][] mat, int x) {
+
+        int n = mat.length;
+        int m = mat[0].length;
+
+        for (int i = 0; i < n; i++) {
+
+            int low = 0;
+            int high = m - 1;
+
+            while (low <= high) {
+
+                int mid = ( low + high ) / 2;
+
+                if (mat[i][mid] == x) {
+                    return true;
+                }
+                else if (mat[i][mid] < x) {
+                    low = mid + 1;
+                }
+                else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return false;
+    }
+}
