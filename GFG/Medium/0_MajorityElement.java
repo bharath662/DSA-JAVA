d8707194668e8a4cfe74e1db4220290f075a4ba2
bharath662 <bharathmedapatla@gmail.com>
2026-08-14 11:44:24 +0530

    Sync GFG - Majority Element (Medium)

---
+++ GFG/Medium/0_MajorityElement.java
@@ -0,0 +1,40 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/majority-element-1587115620/1
+ * Platform     : GFG
+ * Difficulty   : Medium
+ */
+
+class Solution {
+    int majorityElement(int arr[]) {
+
+        int n = arr.length;
+        int candidate = -1;
+        int count = 0;
+ 
+        for (int num : arr) {
+            if (count == 0) {
+                candidate = num;
+                count = 1;
+            } else if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+    
+        count = 0;
+        for (int num : arr) {
+            if (num == candidate) {
+                count++;
+            }
+        }
+
+        
+        if (count > n / 2) {
+            return candidate;
+        } else {
+            return -1;
+        }
+    }
+}
