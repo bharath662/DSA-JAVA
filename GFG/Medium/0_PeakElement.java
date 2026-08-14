e9e2c57d1a253fcc9e8c9afedfb6f84c7b1ca34e
bharath662 <bharathmedapatla@gmail.com>
2026-08-14 12:35:36 +0530

    Sync GFG - Peak element (Medium)

---
+++ GFG/Medium/0_PeakElement.java
@@ -0,0 +1,17 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/peak-element/1
+ * Platform     : GFG
+ * Difficulty   : Medium
+ */
+
+class Solution {
+    public int peakElement(int[] arr) {
+        int n = arr.length;
+        for(int i=0;i<n;i++){
+            if((i==0 || arr[i] >= arr[i-1]) && (i==n-1 || arr[i] >= arr[i+1])){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
