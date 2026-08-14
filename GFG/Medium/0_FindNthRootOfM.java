68999a2017612b075c18b055cc12cfa96f75cb10
bharath662 <bharathmedapatla@gmail.com>
2026-08-14 14:46:52 +0530

    Sync GFG - Find nth root of m (Medium)

---
+++ GFG/Medium/0_FindNthRootOfM.java
@@ -0,0 +1,42 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/find-nth-root-of-m5843/1
+ * Platform     : GFG
+ * Difficulty   : Medium
+ */
+
+class Solution {
+    public int nthRoot(int n, int m) {
+
+        if (m == 0)
+            return 0;
+
+        int low = 1;
+        int high = m;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            long power = 1;
+
+            for (int i = 0; i < n; i++) {
+                power *= mid;
+
+                if (power > m)
+                    break;
+            }
+
+            if (power == m) {
+                return mid;
+            }
+            else if (power < m) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
