9b1e8608e19ad44cdcbc8ee87d67e9898533d46d
bharath662 <bharathmedapatla@gmail.com>
2026-08-19 15:23:42 +0530

    Sync LeetCode - 344. Reverse String (Easy)

---
+++ LeetCode/Easy/344_ReverseString.java
@@ -0,0 +1,21 @@
+/**
+ * Problem Link : https://leetcode.com/problems/reverse-string/
+ * Platform     : LeetCode
+ * Difficulty   : Easy
+ */
+
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+}
