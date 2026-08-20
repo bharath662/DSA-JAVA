3fc15b78b7cb9342e54a6ff70202701f604f7253
bharath662 <bharathmedapatla@gmail.com>
2026-08-20 22:40:48 +0530

    Sync GFG - Linked List End Insertion (Basic)

---
+++ GFG/Basic/0_LinkedListEndInsertion.java
@@ -0,0 +1,33 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/linked-list-insertion-1587115620/1
+ * Platform     : GFG
+ * Difficulty   : Basic
+ */
+
+/*
+class Node{
+    int data;
+    Node next;
+
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+*/
+class Solution {
+    public Node insertAtEnd(Node head, int x) {
+        Node newNode = new Node(x);
+        
+        if(head == null){
+            return newNode;
+        }
+        Node temp = head;
+        while(temp.next != null){
+            temp = temp.next;
+        }
+        temp.next = newNode;
+        return head;
+    }
+    
+}
