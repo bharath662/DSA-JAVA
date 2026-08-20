f3d8029eaf20ce26e4bd486c54b5c470354c9463
bharath662 <bharathmedapatla@gmail.com>
2026-08-20 22:20:31 +0530

    Sync LeetCode - 237. Delete Node in a Linked List (Medium)

---
+++ LeetCode/Medium/237_DeleteNodeInALinkedList.java
@@ -0,0 +1,12 @@
+/**
+ * Problem Link : https://leetcode.com/problems/delete-node-in-a-linked-list/
+ * Platform     : LeetCode
+ * Difficulty   : Medium
+ */
+
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
